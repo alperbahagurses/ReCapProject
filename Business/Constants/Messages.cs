b39ab9159bfe5b63b9c22a983345d8f0807df39c
alperbahagurses <bahagurses23@gmail.com>
2021-02-12 22:06:52 +0300

    Crud Results 12.02.2021

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string CarAdded = "Araba eklendi";
+        public static string BrandNameInvalid = "Marka ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string CarsListed = "Araba listelendi";
+        public static string CarUpdate = "Araba güncellendi";
+        public static string CarDelete = "Araba silindi";
+        public static string ColorUpdate = "Renk güncellendi";
+        public static string ColorDelete = "Renk silindi";
+        public static string ColorAdded = "Renk eklendi";
+        public static string BrandAdded = "Marka eklendi";
+        public static string BrandUpdate = "Marka güncellendi";
+        public static string BrandDelete = "Marka silindi";
+        public static string CarNotAdded = "Araba eklenmedi";
+    }
+}
